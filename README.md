5a5f2aae3e77582e591d0e9ce3d2d21823dbe479
libevm <libevm@proton.me>
2024-05-24 18:06:50 +0900

    cooking

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # reth-anal
 
 Standalone tool for mempool analysis
+
+```
+cargo run -- node
+```
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,116 @@
 use clap::Parser;
 use futures_util::StreamExt;
+use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 use reth::{
-    builder::NodeHandle,
-    cli::Cli,
-    primitives::{Address, IntoRecoveredTransaction},
-    rpc::{
-        compat::transaction::transaction_to_call_request,
-        types::trace::{parity::TraceType, tracerequest::TraceCallRequest},
-    },
-    transaction_pool::TransactionPool,
+    builder::NodeHandle, cli::Cli, dirs::{DataDirPath, MaybePlatformPath, PlatformPath}, primitives::BlockNumberOrTag, providers::{BlockNumReader, BlockReaderIdExt, CanonStateSubscriptions, ChainSpecProvider}, revm::primitives::FixedBytes, transaction_pool::TransactionPool
 };
 use reth_node_ethereum::node::EthereumNode;
+use std::{collections::HashMap, path::Path, sync::Arc};
+use tokio::sync::Mutex;
 
 fn main() {
-    Cli::<RethCliTxpoolExt>::parse()
+    Cli::<RethAnalCliExt>::parse()
         .run(|builder, args| async move {
+
             // launch the node
             let NodeHandle {
-                mut node,
+                node,
                 node_exit_future,
             } = builder.node(EthereumNode::default()).launch().await?;
 
-            // create a new subscription to pending transactions
+            // ChainInfo
+            let chain_spec= node.provider.chain_spec();
+
+            // Get sqlite3 db location
+            let db_path = PlatformPath::<DataDirPath>::default().with_chain(chain_spec.chain).data_dir_path();
+            let db_anal_sqlite3 = db_path.join(args.anal_db).clone();
+            let db_anal_sqlite3 = db_anal_sqlite3.as_path();
+            tracing::info!("DB Anal sqlite3 {:?}", db_anal_sqlite3);
+
+            // create a new subscription to pending transactions and new canon state
             let mut pending_transactions = node.pool.new_pending_pool_transactions_listener();
+            let mut canon_state_notifications = node.provider.subscribe_to_canonical_state();
 
-            // get an instance of the `trace_` API handler
-            let traceapi = node.rpc_registry.trace_api();
+            // Provider clone
+            let provider = node.provider.clone();
 
-            println!("Spawning trace task!");
-            // Spawn an async block to listen for transactions.
+
+            // Simple KV store to denote if transactions are seen in the mempool
+            let seen_txs: Arc<Mutex<HashMap<FixedBytes<32>, bool>>> =
+                Arc::new(Mutex::new(HashMap::new()));
+
+            // Listens to mempool transactions and saves them to KV
+            let seen_txs_mempool = seen_txs.clone();
             node.task_executor.spawn(Box::pin(async move {
                 // Waiting for new transactions
                 while let Some(event) = pending_transactions.next().await {
                     let tx = event.transaction;
-                    println!("Transaction received: {tx:?}");
+                    let tx_hash = tx.hash();
 
-                    if let Some(recipient) = tx.to() {
-                        if args.is_match(&recipient) {
-                            // trace the transaction with `trace_call`
-                            let callrequest =
-                                transaction_to_call_request(tx.to_recovered_transaction());
-                            let tracerequest = TraceCallRequest::new(callrequest)
-                                .with_trace_type(TraceType::Trace);
-                            if let Ok(trace_result) = traceapi.trace_call(tracerequest).await {
-                                let hash = tx.hash();
-                                println!("trace result for transaction {hash}: {trace_result:?}");
+                    // Stores the transaction hash into the KV store
+                    let mut guard = seen_txs_mempool.lock().await;
+                    guard.insert(*tx_hash, true);
+                }
+            }));
+
+            // On new canon state, just update everything (dont care if its a reorg or commit)
+            let seen_txs_canon = seen_txs.clone();
+            node.task_executor.spawn(Box::pin(async move {
+                while let Ok(_) = canon_state_notifications.recv().await {
+                    if let Ok(Some(latest_block)) = provider
+                        .clone()
+                        .block_by_number_or_tag(BlockNumberOrTag::Latest)
+                    {
+                        // Compares with the list of transactions we've seen
+                        let body = latest_block.body;
+
+                        let mut public_txs: Vec<FixedBytes<32>> = Vec::new();
+                        let mut private_txs: Vec<FixedBytes<32>> = Vec::new();
+
+                        // Unblock guard and then saves the public txs
+                        let guard = seen_txs_canon.lock().await;
+                        for tx in body.iter() {
+                            let cur_hash = tx.hash();
+                            if guard.contains_key(&cur_hash) {
+                                public_txs.push(cur_hash);
+                            } else {
+                                private_txs.push(cur_hash);
                             }
                         }
+
+                        // Save to sqlite3
                     }
                 }
             }));
+
+            tracing::info!("Reth Analyzer enabled");
 
             node_exit_future.await
         })
         .unwrap();
 }
 
-/// Our custom cli args extension that adds one flag to reth default CLI.
 #[derive(Debug, Clone, Default, clap::Args)]
-struct RethCliTxpoolExt {
-    /// recipients addresses that we want to trace
-    #[arg(long, value_delimiter = ',')]
-    pub recipients: Vec<Address>,
+struct RethAnalCliExt {
+    /// Analytics database name
+    #[arg(long, default_value = "reth-anal.sqlite3")]
+    pub anal_db: String,
 }
 
-impl RethCliTxpoolExt {
-    /// Check if the recipient is in the list of recipients to trace.
-    pub fn is_match(&self, recipient: &Address) -> bool {
-        self.recipients.is_empty() || self.recipients.contains(recipient)
+// Trait for the new namespace + method
+#[cfg_attr(not(test), rpc(server, namespace = "anal"))]
+#[cfg_attr(test, rpc(server, namespace = "anal"))]
+pub trait RethAnalExtApi {
+    /// Returns the number of transactions in the pool.
+    #[method(name = "blockTxConfidentiality")]
+    fn block_tx_confidentiality(&self) -> RpcResult<usize>;
+}
+
+/// The type that implements the `txpool` rpc namespace trait
+pub struct RethAnalExt {}
+
+impl RethAnalExtApiServer for RethAnalExt {
+    fn block_tx_confidentiality(&self) -> RpcResult<usize> {
+        return Ok(0);
     }
 }
